4ebca1936e5551f0a2b20c2bfd0f34b6f3b9c38e
lhwxl <lhw100501@163.com>
2024-04-22 17:48:56 +0800

    Ctrl+C退出逻辑

--- main.py
+++ main.py
@@ -8,23 +8,34 @@ from sys import exit
 
 import status_code
 
+# 定义日志记录器和日志格式
 log = logging.getLogger(__name__)
 formatter = logging.Formatter('%(asctime)s %(levelname)s [%(filename)s]: %(message)s')
 log.setLevel(logging.DEBUG)
 
-file_handler = logging.handlers.TimedRotatingFileHandler(filename=f"./log.log", encoding="utf-8", when="D")
+# 将日志输出到文本
+# 等级为debug
+file_handler = logging.handlers.TimedRotatingFileHandler(filename=f"./debugLogs/debugLog.log", encoding="utf-8", when="D")
 file_handler.setFormatter(formatter)
 file_handler.setLevel(logging.DEBUG)
 
+# 将日志输出到文本
+# 等级为info
+debug_file_handler = logging.handlers.TimedRotatingFileHandler(filename=f"./logs/Log.log", encoding="utf-8", when="D")
+debug_file_handler.setFormatter(formatter)
+debug_file_handler.setLevel(logging.INFO)
+
+# 将日志输出到屏幕
 stream_handler = logging.StreamHandler()
 stream_handler.setFormatter(formatter)
 stream_handler.setLevel(logging.INFO)
 
+# 将日志处理器添加到记录器
 log.addHandler(file_handler)
+log.addHandler(debug_file_handler)
 log.addHandler(stream_handler)
 
 CONFIGS = load(open("config.json"))
-log.debug("Config loaded")
 
 
 def client_while():
@@ -32,9 +43,8 @@ def client_while():
 
 
 def main():
-	pass
-
-
+	while True:
+		pass
 
 
 if __name__ == '__main__':
@@ -42,10 +52,14 @@ if __name__ == '__main__':
 
 	try:
 		main()
+
+	# Ctrl+C 退出
 	except KeyboardInterrupt:
-		log.debug("Ctrl+C inputted,exitting")
+		log.debug("Ctrl+C inputted")
+
+	# 未捕获的异常将输出
 	except Exception as e:
 		log.error(f"\nUnknown error,error massage: \n{e}")
 	finally:
-		logging.info("服务端退出")
+		log.info("exiting")
 		exit(0)
